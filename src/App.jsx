4b5d3d92686c5e2dffd5585d167a175271c343a4
unknown <matheus.duarte42@gmail.com>
2023-06-11 14:07:52 -0300

    react router com as principais rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './components/Home'
+import Alunos from './components/Alunos'
+import Sobre from './components/Sobre'
 
 function App() {
   return (
     <div>
-      <h1>
-        Crud
-      </h1>
+      <h1>Crud</h1>
+      <BrowserRouter>
+        <ul>
+          <li><Link to='/'>Página Inicial</Link></li>
+          <li><Link to='/alunos'>Cadastro de Alunos</Link></li>
+          <li><Link to='/sobre'>Sobre</Link></li>
+        </ul>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/alunos' element={<Alunos />} />
+          <Route path='/sobre' element={<Sobre />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/Alunos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Alunos = () => {
+  return (
+    <div>Alunos</div>
+  )
+}
+
+export default Alunos
