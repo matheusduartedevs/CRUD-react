c68e1028d7508bfd785ad95db3a6728cdbf2592f
unknown <matheus.duarte42@gmail.com>
2023-06-11 15:27:40 -0300

    desenvolvimento da pag de alunos: state e estilizacao

--- src/components/Alunos.jsx
+++ src/components/Alunos.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
 
-const Alunos = () => {
-  return (
-    <div>Alunos</div>
-  )
+class Alunos extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            alunos: [
+                { 'id': 1, 'nome': 'Miguel Duarte', 'email': 'miguel@teste.com' },
+                { 'id': 2, 'nome': 'Matheus Duarte', 'email': 'matheus@teste.com'},
+                { 'id': 3, 'nome': 'Browser Junior', 'email': 'browserjr@teste.com'}
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Opções</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                    this.state.alunos.map((aluno) => 
+                        <tr>
+                            <td>{aluno.nome}</td>
+                            <td>{aluno.email}</td>
+                            <td>Atualizar Excluir</td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
+        )
+    }
 }
 
 export default Alunos
