23684d022d7e0de801e41290b950bcee629f9a91
unknown <matheus.duarte42@gmail.com>
2023-06-11 14:38:36 -0300

    styles bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,25 @@ import './App.css'
 import Home from './components/Home'
 import Alunos from './components/Alunos'
 import Sobre from './components/Sobre'
+import { Nav } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
       <h1>Crud</h1>
       <BrowserRouter>
-        <ul>
-          <li><Link to='/'>Página Inicial</Link></li>
-          <li><Link to='/alunos'>Cadastro de Alunos</Link></li>
-          <li><Link to='/sobre'>Sobre</Link></li>
-        </ul>
+        <Nav variant="tabs">
+          <Nav.Item>
+            <Nav.Link href="/">Página Inicial</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/alunos">Alunos</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/sobre">Sobre</Nav.Link>
+          </Nav.Item>
+        </Nav>
 
         <Routes>
           <Route path='/' element={<Home />} />
