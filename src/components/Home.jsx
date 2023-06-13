ab7e125b335c2262112f307b1c7071747657d955
unknown <matheus.duarte42@gmail.com>
2023-06-13 08:25:23 -0300

    styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Bem vindo ao Sistema</h1>
+      <p>Utilize os menus para navegar</p>
+    </div>
   )
 }
 
--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Sobre = () => {
   return (
-    <div>Sobre</div>
+    <div>
+      <p>Essa aplicação foi desenvolvida por<b>React</b></p>
+      <p>Um CRUD de alunos em que é possível adicionar, visualizar, atualizar e excluir dados dos alunos.</p>
+    </div>
   )
 }
 
